2e234fd69ac6b84f4d5182dcb7af7362b97dc2e8
BOBKOVSERGEY <sergey_bobkov@inbox.ru>
2024-03-25 17:37:19 +0300

    new resource

---
+++ app/Http/Controllers/Api/V1/CategoryController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Controllers\Api\V1;
+
+use App\Http\Controllers\Controller;
+use App\Http\Resources\CategoryResource;
+use App\Models\Category;
+use Illuminate\Database\Eloquent\Collection;
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\AnonymousResourceCollection;
+
+class CategoryController extends Controller
+{
+    public function index(): AnonymousResourceCollection
+    {
+        return CategoryResource::collection(Category::all());
+    }
+
+    public function show(Category $category): CategoryResource
+    {
+        return new CategoryResource($category);
+    }
+
+    public function list(): AnonymousResourceCollection
+    {
+        return CategoryResource::collection(Category::all());
+    }
+}
---
+++ routes/api.php
@@ -0,0 +1,15 @@
+<?php
+
+use App\Http\Controllers\Api\V1\CategoryController;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+Route::get('/user', function (Request $request) {
+    return $request->user();
+})->middleware('auth:sanctum');
+
+Route::get('/lists/categories', [CategoryController::class, 'list']);
+
+
+Route::get('categories', [CategoryController::class, 'index']);
+Route::get('categories/{category}', [CategoryController::class, 'show']);
