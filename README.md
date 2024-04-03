7c7093f0a3dc4acbd1b67045d6398822b2d2f141
BOBKOVSERGEY <sergey_bobkov@inbox.ru>
2024-04-03 16:53:14 +0300

    set headers in middleware application/json

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 alias sail='sh $([ -f sail ] && echo sail || echo vendor/bin/sail)'
+
+# make middleware
+sail artisan make:middleware AlwaysAcceptJson
+
--- bootstrap/app.php
+++ bootstrap/app.php
@@ -13,7 +13,7 @@ return Application::configure(basePath: dirname(__DIR__))
         apiPrefix: 'api/v1'
     )
     ->withMiddleware(function (Middleware $middleware) {
-        //
+        $middleware->prependToGroup('api', \App\Http\Middleware\AlwaysAcceptJson::class);
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
